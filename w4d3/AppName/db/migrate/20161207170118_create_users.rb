db7f1f7fff9c98e96691862939499856fc42f16f
Ujwala Aaduru <Home@Ujwalas-MacBook-Pro.local>
2016-12-07 09:18:44 -0800

    Rails Auth Homework

---
+++ w4d3/AppName/db/migrate/20161207170118_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+
+      t.timestamps null: false
+
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
