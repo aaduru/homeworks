978aa2f1a627eb41287bae5c64bb4da97b91bdd5
aaduru <ujwala_aaduru@yahoo.com>
2016-12-16 08:54:16 -0800

    webpack tutorial

---
+++ w5d5/entry.js
@@ -0,0 +1,6 @@
+// // document.write("It works.");
+// - document.write("It works.");
+// + document.write(require("./content.js"));
+
++ require("!style!css!./style.css");
+  document.write(require("./content.js"));
